ad954808c08c38b0e2f5bbe0f1341b7b206146c9
FakeGods <w.leszkowicz99@gmail.com>
2024-02-01 21:51:14 +0100

    123

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!1231</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+      <Text style={styles.boldText}>Hello, World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Oпачки</Text>
+        <Text>Oпачки</Text>
+        <Text>Oпачки</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +24,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+    textTransform: 'uppercase'
+  },
+  
 });
