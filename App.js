53cd66a8e326ac5ddae61aa6b0f4c8d8bfa75ed0
FakeGods <w.leszkowicz99@gmail.com>
2024-02-03 15:31:13 +0100

    123

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('shaun');
-  const [person, setPerson] = useState({name: 'mario', age: 40})
+  const [age, setAge] = useState('40')
 
-
-  const clickHandler = () => {
-    setName('Bo');
-    setPerson({name: 'luigi', age: 35});
-
-  }
 
 
 
 
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />
-      </View>
+      <Text>Enter name:</Text>
+      <TextInput style={styles.input} />
+
+      <Text>name: {name}, age: {age}</Text>
+  
     </View>
   );
 }
@@ -34,8 +28,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop: 20,
+  input: {
+    borderWidth: 1,
+    borderColor: 777,
+    padding: 8,
+    margin: 10,
+    width: 200,
   }
+  
   
 });
