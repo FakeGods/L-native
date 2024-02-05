3eaa775eae889ddb02b1ff63a0bfbde041fc8660
FakeGods <w.leszkowicz99@gmail.com>
2024-02-05 13:52:59 +0100

    toDo

--- App.js
+++ App.js
@@ -1,40 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, } from 'react-native';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name:'shaun', key: '1'},
-    { name:'yoshi', key: '2'},
-    { name:'mario', key: '3'},
-    { name:'luigi', key: '4'},
-    { name:'bowser', key: '5'},
-    
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
   ]);
-
-const pressHandler = (id) => {
-  console.log(id);
-  setPeople((prevPeople) => {
-    return prevPeople.filter(person => person.id != id);
-  });
-}
-
-
-
   return (
     
     <View style={styles.container}>
-      <FlatList 
-      numColumns={2}
-      keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      
-      />
+      {/* HEADER */}
+      <View style={styles.content}></View>
+        {/* TO FORM */}
+        <View style={styles.list}></View>
+        <FlatList
+
+        />
     </View>
   );
 }
@@ -44,14 +27,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 40,
-    paddingHorizontal: 20,
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  },
-    item: {
-      marginTop: 24,
-      padding: 30,
-      backgroundColor: 'pink',
-      fontSize: 24,
-    }
-  });
+  }
+});
