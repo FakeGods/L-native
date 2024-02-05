4f8d83204eff59a47d3cd3adf0631f0b8231131f
FakeGods <w.leszkowicz99@gmail.com>
2024-02-05 12:48:44 +0100

    123

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const pressHandler = (id) => {
   console.log(id);
   setPeople((prevPeople) => {
-    return prevPeople.filter(person => person.id != id)
+    return prevPeople.filter(person => person.id != id);
   })
 }
 
