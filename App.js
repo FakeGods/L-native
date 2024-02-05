bfe692162b0720a3b829ecdde2f012c71dd8f672
FakeGods <w.leszkowicz99@gmail.com>
2024-02-05 19:35:17 +0100

    toDo

--- App.js
+++ App.js
@@ -5,20 +5,19 @@ import { StyleSheet, Text, View, FlatList, } from 'react-native';
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
-    { text: 'create an app', key: '2' },
+    { text: 'create an app' , key: '2' },
     { text: 'play on the switch', key: '3' }
   ]);
   return (
-    
     <View style={styles.container}>
       {/* HEADER */}
       <View style={styles.content}></View>
         {/* TO FORM */}
         <View style={styles.list}></View>
         <FlatList
-         data={todos}
-         renderItem={({ item }) => (
-          <Text>{item.text}</Text>
+          data={todos}
+           renderItem={({ item }) => (
+            <Text>{item.text}</Text>
          )}
         />
     </View>
@@ -30,5 +29,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 40,
-  }
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+
 });
