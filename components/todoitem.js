c1470e81bd99d3329c0880386ab00a1c99043c44
FakeGods <w.leszkowicz99@gmail.com>
2024-02-05 20:40:39 +0100

    hehe

--- components/todoitem.js
+++ components/todoitem.js
@@ -4,10 +4,17 @@ import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 export default function TodoItem({ item }) {
     return (
         <TouchableOpacity>
-            <Text sty>{item.text}</Text>
+            <Text style={styles.item}>{item.text}</Text>
         </TouchableOpacity>
     )};
 
     const styles = StyleSheet.create({
-        
+        item: {
+            padding: 16,
+            marginTop: 16,
+            borderColor: '#bbb',
+            borderWidth: 1,
+            borderStyle: 'dashed',
+            borderRadius: 10,
+        }
     })
