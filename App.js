0286875b78b8edcbd8a5ff6f7d3060580500e499
FakeGods <w.leszkowicz99@gmail.com>
2024-02-05 12:42:18 +0100

    555

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -12,7 +12,12 @@ export default function App() {
     
   ]);
 
-
+const pressHandler = (id) => {
+  console.log(id);
+  setPeople((prevPeople) => {
+    return prevPeople.filter(person => person.id != id)
+  })
+}
 
 
 
@@ -24,7 +29,9 @@ export default function App() {
       keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
         )}
       
       />
